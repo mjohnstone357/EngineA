bbedc6c600617e258ad3adc0279c8ad2ad8c7243
mjohnstone357 <mjohnstone357@gmail.com>
2013-07-20 17:52:40 +0100

    Hello, World

---
+++ EngineA/src/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * @author Matthew Johnstone
+ *         Date: 20/07/13
+ *         Time: 17:51
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+
+}
