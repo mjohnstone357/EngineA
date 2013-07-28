ef5c95d2695679781a96bc9975a06cbfad730fa7
mjohnstone357 <mjohnstone357@gmail.com>
2013-07-28 16:01:51 +0100

    Removed hacked together Engine code.

--- EngineA/src/engine/Engine.java
+++ EngineA/src/engine/Engine.java
@@ -28,56 +28,12 @@ public class Engine {
 
     }
 
-    public void tick() {
-
-        Bitstring randomBitstring = bitstringGenerator.generateRandomBitstring();
-
-        Model randomModel1 = modelGenerator.createRandomModel();
-        Model randomModel2 = modelGenerator.createRandomModel();
-
-        models.add(randomModel1);
-        models.add(randomModel2);
-
-        doTokens(randomBitstring);
-
-    }
-
-    private void doTokens(Bitstring randomBitstring) {
-
-        final int actualFitness = problem.fitness(randomBitstring);
-
-        Model worstModel = models.iterator().next();
-        int maxDelta = Math.abs(actualFitness - worstModel.getPredictedFitness(randomBitstring));
-
-        for (Model model : models) {
-            int modelPredictedFitness = model.getPredictedFitness(randomBitstring);
-
-            final int delta = Math.abs(actualFitness - modelPredictedFitness);
-
-            if (delta > maxDelta) {
-                maxDelta = delta;
-                worstModel = model;
-            }
-        }
-
-        models.remove(worstModel);
-    }
-
-    public void run() {
-        while (models.size() < 10) {
-            tick();
-        }
-        System.out.println();
-    }
-
     public List<Model> getModels() {
         return models;
     }
 
     public static void main(String[] args) {
         Engine engine = new Engine();
-
-        engine.run();
 
         Bitstring opt = new Bitstring(8, "11000111");
 
